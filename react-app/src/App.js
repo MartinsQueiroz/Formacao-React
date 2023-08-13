b250c6950d8571043b3f74debfaf71827a5084b7
lucas martins <lucasmqueiroz@outlook.com>
2023-08-13 01:15:57 -0300

    hora de codar

---
+++ react-app/src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+
+function MyApp() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1 className='head-1'>Welcome to my app</h1>
+      <h2 className='head-2'>Welcome to my app</h2>
+      <h3 className='head-3'>Welcome to my app</h3>
+      <h4 className='head-4'>Welcome to my app</h4>
+      <h5 className='head-5'>Welcome to my app</h5>
+      <h6 className='head-5'>Welcome to my app</h6>
+      <p className='paragraph'>Welcome to my app</p>
+      <p className='paragraph'>Welcome to my app</p>
+      <span className='text'>Welcome to my app</span>
+      <small className='word'>Welcome to my app</small>
+
+      </header>
+    </div>
+  );
+}
+
+export default MyApp;
