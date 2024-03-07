ec961aa27253b1822c667077402ef82166608748
lucas martins <lucasmqueiroz@outlook.com>
2024-03-07 19:53:42 -0300

    movendo my-app-forms

---
+++ my-app-forms/src/App.js
@@ -0,0 +1,18 @@
+import Form from "./Form";
+import Header from "./Header";
+import Foot from "./Foot"
+import "./styles.css";
+import "./footer.css"
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Form />
+      <Foot />
+    </div>
+  );
+}
+
+
+
---
+++ my-app-forms/src/Form.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+class Form extends React.Component {
+  render() {
+    return (
+      <form>
+        <div>
+          <label>Nome:</label>
+          <input
+            type="text"
+            name="Nome"
+            id="Nome" //required="true"
+            onChange={(event) => console.log(event)}
+          />
+          <label>CPF:</label>
+          <input
+            type="text"
+            name="CPF"
+            id="CPF" //required="true"
+            onChange={(event) => console.log(event)}
+          />
+        </div>
+        <div>
+          <label>CEP:</label>
+          <input
+            type="text"
+            name="CEP"
+            id="CEP" //required="true"
+            onChange={(event) => console.log(event)}
+          />
+          <label>RUA:</label>
+          <input
+            type="text"
+            name="RUA"
+            id="RUA" //required="true"
+            onChange={(event) => console.log(event)}
+          />
+          <label>N°:</label>
+          <input
+            type="text"
+            name="NUMERO"
+            id="NUMERO" //required="true"
+            onChange={(event) => console.log(event)}
+          />
+        </div>
+        <div onChange={(event) => console.log(event.target.value)}>
+          <label>Gênero:</label>
+          <input type="radio" name="gender" value="Masculino" defaultChecked />
+          Masculino
+          <input type="radio" name="gender" value="Feminino" />
+          Feminino
+          <input type="radio" name="gender" value="Outros" />
+          Outros
+        </div>
+        <div>
+          <label>Mensagem:</label>
+          <textarea type="text" placeholder="Deixe sua mensagem aqui..." />
+        </div>
+      </form>
+    );
+  }
+}
+
+export default Form;
