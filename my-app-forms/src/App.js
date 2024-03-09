678646b456d38c6c3e837ebb129256d82103db2f
lucas martins <lucasmqueiroz@outlook.com>
2024-03-09 11:26:59 -0300

    modificando o react forms

--- my-app-forms/src/App.js
+++ my-app-forms/src/App.js
@@ -1,18 +1,25 @@
-import Form from "./Form";
-import Header from "./Header";
-import Foot from "./Foot"
-import "./styles.css";
-import "./footer.css"
+import logo from './logo.svg';
+import './App.css';
 
-export default function App() {
+function App() {
   return (
     <div className="App">
-      <Header />
-      <Form />
-      <Foot />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
-
-
+export default App;
