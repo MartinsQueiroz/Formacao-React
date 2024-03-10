22914a1c6511f4bd997c2b07de8d12a3e4b93c1b
lucas martins <lucasmqueiroz@outlook.com>
2024-03-10 11:23:49 -0300

    atualiza app

--- my-app-forms/src/components/Footer/Footer.js
+++ my-app-forms/src/components/Footer/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./footer.css";
 
 class Foot extends React.Component {
     render() {
--- my-app-forms/src/components/Form/Form.js
+++ my-app-forms/src/components/Form/Form.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./form.css";
 
 class Form extends React.Component {
   render() {
