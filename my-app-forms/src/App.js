ab35b51bc4723175378bbec3f0d0517b74ef1e00
lucas martins <lucasmqueiroz@outlook.com>
2024-03-14 18:30:59 -0300

    atualiza componente app

--- my-app-forms/src/App.js
+++ my-app-forms/src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import Header from './components/Header/Header'
 import Form from './components/Form/Form'
 import Footer from './components/Footer/Footer'
+import Button from './components/Button/Button';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Form />
+      <Button />
       <Footer />
     </div>
   );
---
+++ my-app-forms/src/components/Button/Button,.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Button.css"
+class Button extends Component {
+    render(){
+        return(
+            <div>
+                <button type='sumbit'>Enviar</button>
+            </div>
+        );
+    }
+}
+
+export default Button;
